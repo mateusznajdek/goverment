d30862dfb0e7ed86830be9af4e7525516234ce1b
Mateusz Najdek <mateusz14321@gmail.com>
2016-12-19 21:00:27 +0100

    Update Main.java

--- src/com/goverment/Main.java
+++ src/com/goverment/Main.java
@@ -14,7 +14,10 @@ public class Main {
             ParseTripsAndSpendings();
         }
 
-        
+        Funkcja ArgumentParser jak sama nazwa wskazuje, bd parsowala argumenty wywolania programu, nastepnie jest tworzony nowyobiekt parlament dla danej kadencji sejmu, jak bd wiecej kadencji, bd wiecej parlamentow
+nastepnie dla kazdej kadencji sejmu bd parsowac czlonkow, czyli poslow, majac dla kazdej kadencji sejmu liste poslow, przejdziemy po tej liscie i dla bd zliczac dla kazdego posla jego wyjazdy (rzeczy zwiazane z wyjazdami) i z wydatkami 
+na koniec powypisuje co trzeba i tyle :)
+
 
     }
 }
